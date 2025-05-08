7309650b002e9ee5222669dddbf7665603b8a1c6
kakani17481a0566 <84347625+kakani17481a0566@users.noreply.github.com>
2025-05-08 14:25:54 +0530

    Org

---
+++ NeuroPi/ViewModel/Organization/OrganizationInputModel.cs
@@ -0,0 +1,10 @@
+﻿namespace NeuroPi.ViewModel.Organization
+{
+    public class OrganizationInputModel
+    {
+        public string Name { get; set; }
+        public int? ParentId { get; set; }
+        public int TenantId { get; set; }
+    }
+
+}
---
+++ NeuroPi/ViewModel/Organization/OrganizationViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace NeuroPi.ViewModel.Organization
+{
+    public class OrganizationViewModel
+    {
+        public int OrganizationId { get; set; }
+        public string Name { get; set; }
+        public int? ParentId { get; set; }
+        public int TenantId { get; set; }
+    }
+}
