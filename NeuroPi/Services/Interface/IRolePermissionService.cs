365eb341f863f78cae33cd04da931660980bab25
KiranShyam <135624474+KiranShyam@users.noreply.github.com>
2025-05-15 11:28:19 +0530

    Role Permission TenantID Added

--- NeuroPi/Services/Interface/IRolePermissionService.cs
+++ NeuroPi/Services/Interface/IRolePermissionService.cs
@@ -3,11 +3,23 @@
 namespace NeuroPi.UserManagment.Services.Interface
 {
     public interface IRolePermissionService
+
     {
-        RolePermissionResponseVM AddRolePermission(RolePermissionRequestVM rolePermission);
-        RolePermissionResponseVM UpdateRolePermissionById(int id, RolePermissionVM rolePermission);
-        bool DeleteById(int id);
         List<RolePermissionResponseVM> GetAllRolePermissions();
+
         RolePermissionResponseVM GetRolePermissionById(int id);
+
+        RolePermissionResponseVM GetRolePermissionByIdAndTenantId(int id, int tenantId);
+
+        List<RolePermissionResponseVM> GetRolePermissionByTenantId(int tenantId);
+
+        RolePermissionResponseVM AddRolePermission(RolePermissionRequestVM rolePermission);
+
+        RolePermissionResponseVM UpdateRolePermissionByIdAndTenantId(int id, int tenantId, RolePermissionVM rolePermission); 
+
+        bool DeleteByIdAndTenantId(int id, int tenantId);
+
+        
     }
 }
+ 
