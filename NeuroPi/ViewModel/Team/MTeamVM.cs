bf7d4b74dda033a6c2166e1b31f8d77bc382fa50
DELL <DELL@DESKTOP-K3NKTL0>
2025-05-15 10:26:15 +0530

    Updated to tenantId

--- NeuroPi/ViewModel/Team/MTeamVM.cs
+++ NeuroPi/ViewModel/Team/MTeamVM.cs
@@ -1,9 +1,31 @@
-﻿namespace NeuroPi.UserManagment.ViewModel.Team
+﻿using NeuroPi.UserManagment.Model;
+
+namespace NeuroPi.UserManagment.ViewModel.Team
 {
     public class MTeamVM
     {
         public int TeamId { get; set; }
         public string Name { get; set; }
         public int TenantId { get; set; }
+
+        public static MTeamVM ToViewModel(MTeam team)
+        {
+            return new MTeamVM
+            {
+                TeamId = team.TeamId,
+                Name = team.Name,
+                TenantId = team.TenantId,
+            };
+        }
+        public static List<MTeamVM> ToViewModelList(List<MTeam> list)
+        {
+            List<MTeamVM> teamsList= new List<MTeamVM>();
+            foreach(MTeam team in list) 
+            {
+             teamsList.Add(ToViewModel(team));  
+
+            }
+            return teamsList;
+        }
     }
 }
