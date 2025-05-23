966ea89697852417cfc7749e479753aea565d30f
kakani17481a0566 <84347625+kakani17481a0566@users.noreply.github.com>
2025-05-23 08:14:33 +0530

    Updated:

---
+++ SchoolManagement/Data/SchoolManagementDb.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace NeuroPi.UserManagment.Data
+{
+    public class SchoolManagementDb : DbContext
+    {
+        public SchoolManagementDb(DbContextOptions<SchoolManagementDb> options) : base(options) { }
+
+      
+    }
+}
