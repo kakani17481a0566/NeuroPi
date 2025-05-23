aedb1edf37c3fa78b3560946c4a8ee7996e053b8
kakani17481a0566 <84347625+kakani17481a0566@users.noreply.github.com>
2025-05-23 13:15:21 +0530

    Updated:

--- SchoolManagement/Program.cs
+++ SchoolManagement/Program.cs
@@ -1,15 +1,40 @@
 using Microsoft.EntityFrameworkCore;
+using NeuroPi.UserManagment.Data;
+using NeuroPi.UserManagment.Services.Implementation;
+using NeuroPi.UserManagment.Services.Interface;
 using SchoolManagement.Data;
 using SchoolManagement.Services.Implementation;
 using SchoolManagement.Services.Interface;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.NeuroPi.UserManagment
+// Register application services
+builder.Services.AddScoped<ITenantService, TenantServiceImpl>();
+builder.Services.AddScoped<IDepartmentService, DepartmentServiceImpl>();
+builder.Services.AddScoped<IGroupService, GroupServiceImpl>();
+builder.Services.AddScoped<IOrganizationService, OrganizationImpl>();
+builder.Services.AddScoped<IGroupUserService, GroupUserServiceImpl>();
+builder.Services.AddScoped<IRolePermissionService, RolePermissionServiceImpl>();
+builder.Services.AddScoped<ITeamService, TeamServiceImpl>();
+builder.Services.AddScoped<IRoleService, RoleServiceImpl>();
+builder.Services.AddScoped<ITeamUserService, TeamUserServiceImpl>();
+builder.Services.AddScoped<IPermissionService, PermissionServiceImpl>();
+builder.Services.AddScoped<IUserDepartmentService, UserDepartmentServiceImpl>();
+builder.Services.AddScoped<IConfigService, ConfigServiceImpl>();
+builder.Services.AddScoped<IUserRolesService, UserRolesServiceImpl>();
+builder.Services.AddScoped<IUserService, UserServiceImpl>();
+
+
 
 builder.Services.AddScoped<IContactService, ContactServiceImpl>();
 
+builder.Services.AddDbContext<NeuroPiDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddDbContext<SchoolManagementDb>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
