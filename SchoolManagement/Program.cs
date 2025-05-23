2296219df55fe5cb4f62251cfdc439e4119eecf2
kakani17481a0566 <84347625+kakani17481a0566@users.noreply.github.com>
2025-05-23 14:12:16 +0530

    Updated:

--- SchoolManagement/Program.cs
+++ SchoolManagement/Program.cs
@@ -10,20 +10,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.NeuroPi.UserManagment
 // Register application services
-builder.Services.AddScoped<ITenantService, TenantServiceImpl>();
-builder.Services.AddScoped<IDepartmentService, DepartmentServiceImpl>();
-builder.Services.AddScoped<IGroupService, GroupServiceImpl>();
-builder.Services.AddScoped<IOrganizationService, OrganizationImpl>();
-builder.Services.AddScoped<IGroupUserService, GroupUserServiceImpl>();
-builder.Services.AddScoped<IRolePermissionService, RolePermissionServiceImpl>();
-builder.Services.AddScoped<ITeamService, TeamServiceImpl>();
-builder.Services.AddScoped<IRoleService, RoleServiceImpl>();
-builder.Services.AddScoped<ITeamUserService, TeamUserServiceImpl>();
-builder.Services.AddScoped<IPermissionService, PermissionServiceImpl>();
-builder.Services.AddScoped<IUserDepartmentService, UserDepartmentServiceImpl>();
-builder.Services.AddScoped<IConfigService, ConfigServiceImpl>();
-builder.Services.AddScoped<IUserRolesService, UserRolesServiceImpl>();
-builder.Services.AddScoped<IUserService, UserServiceImpl>();
+//builder.Services.AddScoped<ITenantService, TenantServiceImpl>();
+//builder.Services.AddScoped<IDepartmentService, DepartmentServiceImpl>();
+//builder.Services.AddScoped<IGroupService, GroupServiceImpl>();
+//builder.Services.AddScoped<IOrganizationService, OrganizationImpl>();
+//builder.Services.AddScoped<IGroupUserService, GroupUserServiceImpl>();
+//builder.Services.AddScoped<IRolePermissionService, RolePermissionServiceImpl>();
+//builder.Services.AddScoped<ITeamService, TeamServiceImpl>();
+//builder.Services.AddScoped<IRoleService, RoleServiceImpl>();
+//builder.Services.AddScoped<ITeamUserService, TeamUserServiceImpl>();
+//builder.Services.AddScoped<IPermissionService, PermissionServiceImpl>();
+//builder.Services.AddScoped<IUserDepartmentService, UserDepartmentServiceImpl>();
+//builder.Services.AddScoped<IConfigService, ConfigServiceImpl>();
+//builder.Services.AddScoped<IUserRolesService, UserRolesServiceImpl>();
+//builder.Services.AddScoped<IUserService, UserServiceImpl>();
 
 
 
