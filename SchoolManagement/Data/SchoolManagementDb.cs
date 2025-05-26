998b2fa876fdfac4ee171b0a5c452b85f181709e
kakani17481a0566 <84347625+kakani17481a0566@users.noreply.github.com>
2025-05-26 14:59:15 +0530

    accepy

--- SchoolManagement/Data/SchoolManagementDb.cs
+++ SchoolManagement/Data/SchoolManagementDb.cs
@@ -8,10 +8,15 @@ namespace SchoolManagement.Data
     public class SchoolManagementDb : DbContext
     {
         public SchoolManagementDb(DbContextOptions<SchoolManagementDb> options) : base(options) { }
-
         public DbSet<MContact> Contacts { get; set; }
         public DbSet<MInstitution> Institutions { get; set; }
-
-     
+        public DbSet<MMasterType> MasterTypes { get; set; }
+        public DbSet<MMaster> Masters { get; set; }
+        public DbSet<MItemHeader> ItemHeaders { get; set; }
+        public DbSet<MItem> Items { get; set; }
+        public DbSet<MLibBookRecord> LibBookRecords { get; set; }
+        public DbSet<MBook> Books { get; set; }
+        public DbSet<MAccount> Accounts { get; set; }
+        public DbSet<MTransaction> Transactions { get; set; }
     }
 }
