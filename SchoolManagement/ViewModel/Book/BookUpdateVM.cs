b92c0ad7cad328f4881eb9799fa4b346cc96f4e2
kakani17481a0566 <84347625+kakani17481a0566@users.noreply.github.com>
2025-05-28 11:11:58 +0530

    Books

---
+++ SchoolManagement/ViewModel/Book/BookUpdateVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SchoolManagement.ViewModel.Book
+{
+    public class BookUpdateVM
+    {
+        public string Name { get; set; }
+        public int? ParentId { get; set; }
+        public int? UserId { get; set; }
+        public int? InstitutionId { get; set; }
+        public int? BooksTypeId { get; set; }
+        public int UpdatedBy { get; set; }
+    }
+}
