a4fa66e10079f23a354e1fd727850504b77a8a62
KiranShyam <135624474+KiranShyam@users.noreply.github.com>
2025-06-03 09:49:46 +0530

    Added conflict

---
+++ SchoolManagement/ViewModel/PrefixSuffix/PrefixSuffixUpdateVM.cs
@@ -0,0 +1,13 @@
+﻿namespace SchoolManagement.ViewModel.PrefixSuffix
+{
+    public class PrefixSuffixUpdateVM
+    {
+       
+        public string? Prefix { get; set; } = null!;
+        public string? Suffix { get; set; }
+        public int Length { get; set; }    
+        public int UpdatedBy { get; set; }
+
+    }
+}
+ 
