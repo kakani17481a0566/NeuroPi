77d48e5efaf44c1f73189e6d0a4a3874bf3a8a0a
kakani17481a0566 <84347625+kakani17481a0566@users.noreply.github.com>
2025-06-17 09:22:36 +0530

    Updated:

---
+++ SchoolManagement/Model/MTimeTableAssessment.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using NeuroPi.UserManagment.Model;
+
+namespace SchoolManagement.Model
+{
+    [Table("time_table_assessments")]
+    public class MTimeTableAssessment : MBaseModel
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("time_table_id")]
+        public int TimeTableId { get; set; }
+
+        [Column("assessment_id")]
+        public int AssessmentId { get; set; }
+
+        [Column("status")]
+        public string Status { get; set; }
+
+        [Column("tenant_id")]
+        public int TenantId { get; set; }
+
+        [ForeignKey(nameof(TenantId))]
+        public virtual MTenant Tenant { get; set; }
+    }
+}
