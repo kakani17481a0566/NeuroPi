95da6cc01f7fbd46486853ca60768fa830997f17
kakani17481a0566 <84347625+kakani17481a0566@users.noreply.github.com>
2025-06-26 12:24:32 +0530

    MStudentAttendance

---
+++ SchoolManagement/Model/MStudentAttendance.cs
@@ -0,0 +1,44 @@
+﻿using NeuroPi.UserManagment.Model;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SchoolManagement.Model
+{
+    [Table("student_attendance")]
+    public class MStudentAttendance : MBaseModel
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("student_id")]
+        [ForeignKey("Student")]
+        public int StudentId { get; set; }
+        public virtual MStudent Student { get; set; }
+
+        [Required]
+        [Column("user_id")]
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        public virtual MUser User { get; set; }
+
+        [Required]
+        [Column("date")]
+        public DateTime Date { get; set; }
+
+        [Required]
+        [Column("from_time")]
+        public TimeSpan FromTime { get; set; }
+
+        [Required]
+        [Column("to_time")]
+        public TimeSpan ToTime { get; set; }
+
+        [Column("branch_id")]
+        [ForeignKey("Branch")]
+        public int? BranchId { get; set; }
+        public virtual MBranch Branch { get; set; }
+    }
+}
