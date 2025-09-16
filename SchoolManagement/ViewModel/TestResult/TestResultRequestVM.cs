4ac533d82e90e12bfd3c3aaa692217488feb40f4
SaivardhanClasses <bablugone4@gmail.com>
2025-09-16 13:42:38 +0530

    vms added

---
+++ SchoolManagement/ViewModel/TestResult/TestResultRequestVM.cs
@@ -0,0 +1,29 @@
+﻿using SchoolManagement.Model;
+
+namespace SchoolManagement.ViewModel.TestResult
+{
+    public class TestResultRequestVM
+    {
+        public int studentId { get; set; }
+
+        public int testContentId { get; set; }
+        public string result { get; set; }
+
+        public int testId { get; set; }
+
+        public int relationId { get; set; }
+
+        public static MTestResult ToModel(TestResultRequestVM testResultRequest)
+        {
+            return new MTestResult()
+            {
+                StudentId = testResultRequest.studentId,
+                TestContentId = testResultRequest.testContentId,
+                Result = testResultRequest.result,
+                TestId = testResultRequest.testId,
+                RelationId = testResultRequest.relationId,
+
+            };
+        }
+    }
+}
---
+++ SchoolManagement/ViewModel/TestResult/TestResultResponseVM.cs
@@ -0,0 +1,14 @@
+﻿namespace SchoolManagement.ViewModel.TestResult
+{
+    public class TestResultResponseVM
+    {
+        public int relationId { get; set; }
+
+        public int testId { get; set; }
+
+        public string name { get; set; }
+
+        public  string  result { get; set; }
+        public byte[] url { get; set; }
+    }
+}
