1e22310d31a88250876d3e34bde672a488a1a361
kakani17481a0566 <84347625+kakani17481a0566@users.noreply.github.com>
2025-11-05 18:23:07 +0530

    Create MMealPlan.cs

---
+++ NeuroPi.Nutrition/Model/MMealPlan.cs
@@ -0,0 +1,33 @@
+﻿using NeuroPi.CommonLib.Model;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NeuroPi.Nutrition.Model
+{
+    [Table("nut_meal_plan")]
+    public class MMealPlan : MBaseModel
+    {
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("user_id")]
+        public int UserId { get; set; }
+
+        [Column("meal_type_id")]
+        [ForeignKey("MealType")]
+        public int MealTypeId { get; set; }
+
+        [Column("date")]
+        public DateOnly Date { get; set; }
+
+        [Column("nutritional_item_id")]
+        public int NutritionalItemId { get; set; }
+
+        [Column("qty")]
+        public int Quantity { get; set; }
+
+        [Column("tenant_id")]
+        public int TenantId { get; set; }
+
+        public MMealType MealType { get; set; }
+    }
+}
