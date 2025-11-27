0fc2db9fd277ff5a6962adf4dd0322f8b46f37f4
KiranShyam <135624474+KiranShyam@users.noreply.github.com>
2025-11-27 21:35:37 +0530

    model change

--- NeuroPi.Nutrition/Data/NeutritionDbContext.cs
+++ NeuroPi.Nutrition/Data/NeutritionDbContext.cs
@@ -47,9 +47,12 @@ namespace NeuroPi.Nutrition.Data
 
         public DbSet<MNutritionalFocusItem> NutritionalFocusItem { get; set; }
 
+<<<<<<< Updated upstream
 
         public DbSet<MUserFeedback> UserFeedback { get; set; }
 
+=======
+>>>>>>> Stashed changes
         public DbSet<MResourceType> ResourceTypes { get; set; }
         public DbSet<MResourceMaster> ResourceMasters { get; set; }
         public DbSet<MResourceInstruction> ResourceInstructions { get; set; }
